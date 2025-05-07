01d3d7d554da645eaf75b8b19ad62cfa771458f0
João Marcos Moreira Laudares <83354691+Relampago14@users.noreply.github.com>
2025-05-07 12:17:06 -0300

    Adição das questões 4, 5 e 6 da Lista 2
    Adicionei as questões 4, 5 e 6 da Lista 2 de exercícios, completando-a.

---
+++ Lista_2/q4/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program{
+    public class Dependente{
+        public int NumeroCota { get; set; }
+        public string NomeDependente { get; set; }
+        public DateTime DataNascimento { get; set; }
+
+        public Dependente(int numeroCota, string nomeDependente, DateTime dataNascimento){
+            NumeroCota = numeroCota;
+            NomeDependente = nomeDependente;
+            DataNascimento = dataNascimento;
+        }
+    }
+    public class Socio{
+        public int NumeroCota { get; set; }
+        public string NomeSocio { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public DateTime DataAquisicao{ get; set; }
+        public List<Dependente> Dependentes { get; set; }
+
+        public Socio(int numeroCota, string nomeSocio, DateTime dataNascimento, DateTime dataAquisicao){
+            NumeroCota = numeroCota;
+            NomeSocio = nomeSocio;
+            DataNascimento = dataNascimento;
+            DataAquisicao = dataAquisicao;
+            Dependentes = new List<Dependente>();
+        }
+
+    }
+    public class Socios{
+        public List<Socio> ListaSocios = new List<Socio>();
+
+        public void CadastrarSocio(){
+            Console.Write("Digite o número da cota: ");
+            int numeroCota = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite o nome do sócio: ");
+            string nomeSocio = Console.ReadLine();
+
+            Console.Write("Data de Nascimento (dd/mm/aaaa): ");
+            DateTime dataNascimento = DateTime.Parse(Console.ReadLine());
+
+            Console.Write("Data de Aquisição da Cota (dd/mm/aaaa): ");
+            DateTime dataAquisicao = DateTime.Parse(Console.ReadLine());
+
+            Socio socio = new Socio(numeroCota, nomeSocio, dataNascimento, dataAquisicao);
+            ListaSocios.Add(socio);
+        }
+
+        public void CadastrarDependente(Socio socio){
+            Console.Write("Digite o número da cota: ");
+            int numeroCota = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite o nome do sócio: ");
+            string nomeDependente = Console.ReadLine();
+
+            Console.Write("Data de Nascimento (dd/mm/aaaa): ");
+            DateTime dataNascimento = DateTime.Parse(Console.ReadLine());
+            Dependente dependente = new Dependente(numeroCota, nomeDependente, dataNascimento);
+
+            socio.Dependentes.Add(dependente);
+        }
+
+        public void ListarSocios(){
+            foreach (Socio socio in ListaSocios){
+                Console.WriteLine($"{socio.NumeroCota} {socio.NomeSocio} {socio.DataNascimento} {socio.DataAquisicao}");
+                foreach (Dependente dependente in socio.Dependentes){
+                    Console.WriteLine($"{dependente.NumeroCota} {dependente.NomeDependente} {socio.DataNascimento}");
+                }
+            }
+        }
+
+        public void RemoverSocio(){
+            Console.Write("Digite o número da cota do sócio a ser removido: ");
+            int numeroCota = int.Parse(Console.ReadLine());
+
+            Socio socio = ListaSocios.Find(s => s.NumeroCota == numeroCota);
+            if (socio != null){
+                ListaSocios.Remove(socio);
+                Console.WriteLine($"Sócio {socio.NomeSocio} removido com sucesso!\n");
+            }
+            else{
+                Console.WriteLine("Erro: Sócio não encontrado.\n");
+            }
+        }
+    }
+    public static void Main(){
+        Socios socios = new Socios();
+        socios.CadastrarSocio();
+
+        socios.CadastrarDependente(socios.ListaSocios[0]);
+
+        socios.ListarSocios();
+
+        socios.RemoverSocio();
+
+        socios.ListarSocios();
+    }
+}
---
+++ Lista_2/q6/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+class Livro{
+    public string Titulo { get; set; }
+    public string Autor { get; set; }
+    public string Genero { get; set; }
+    public int Ano { get; set; }
+
+    public Livro(string titulo, string autor, string genero, int ano){
+        Titulo = titulo;
+        Autor = autor;
+        Genero = genero;
+        Ano = ano;
+    }
+
+    public string ObterTitulo(){
+        return Titulo;
+    }
+
+    public string ObterAutor(){
+        return Autor;
+    }
+
+    public string ObterGenero(){
+        return Genero;
+    }
+
+    public int ObterAno(){
+        return Ano;
+    }
+
+    public bool IsModernismo(){
+        return Ano >= 1930 && Ano <= 1945;
+    }
+
+    public bool IsBarroco(){
+        return Ano >= 1601 && Ano <= 1768;
+    }
+
+    public void ExibirLivro(){
+        Console.WriteLine("\nDetalhes do Livro:");
+        Console.WriteLine($"Título: {Titulo}");
+        Console.WriteLine($"Autor: {Autor}");
+        Console.WriteLine($"Gênero: {Genero}");
+        Console.WriteLine($"Ano de Publicação: {Ano}");
+
+        if (IsModernismo())
+            Console.WriteLine("Esse livro pertence ao Modernismo.");
+
+        if (IsBarroco())
+            Console.WriteLine("Esse livro pertence ao Barroco.");
+    }
+}
+
+class Progra{
+    public static void Main(){
+        Console.WriteLine("Digite o título do livro: ");
+        string titulo = Console.ReadLine();
+
+        Console.WriteLine("Digite o autor do livro: ");
+        string autor = Console.ReadLine();
+
+        Console.WriteLine("Digite o gênero do livro: ");
+        string genero = Console.ReadLine();
+
+        Console.WriteLine("Digite o ano de publicação do livro: ");
+        int ano = int.Parse(Console.ReadLine());
+
+        Livro livro = new Livro(titulo, autor, genero, ano);
+
+        livro.ExibirLivro();
+    }
+}
