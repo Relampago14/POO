53350146b84d12e3f02792a09738470cb7b661da
João Marcos Moreira Laudares <83354691+Relampago14@users.noreply.github.com>
2025-05-07 12:05:10 -0300

    Adição da Questão 4 da Lista 8
    Adicionei a questão 4 da lista 8 de exercícios, completando-a.

---
+++ Lista_8/q4/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Livro{
+    public string Titulo;
+    public string Autor;
+
+    public Livro(string titulo, string autor){
+        Titulo = titulo;
+        Autor = autor;
+    }
+
+    public class Publicacao{
+        public string Editora;
+        public int Ano;
+
+        public Publicacao(string editora, int ano){
+            Editora = editora;
+            Ano = ano;
+        }
+    }
+}
+
+class Program{
+    public static void Main(){
+        Livro livro = new Livro("The Last of Us", "Neil Druckman");
+
+        Livro.Publicacao publicacao = new Livro.Publicacao("Naugthy Dog", 2013);
+
+        Console.WriteLine($"Livro {livro.Titulo} - Autor {livro.Autor}");
+        Console.WriteLine($"Publicação {publicacao.Editora} - Ano {publicacao.Ano}");
+    }
+}
