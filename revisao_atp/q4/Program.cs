4e259ae5fc61bdbaac8f41ff45a0ce96b72df23c
João Marcos Moreira Laudares <83354691+Relampago14@users.noreply.github.com>
2025-05-07 12:10:29 -0300

    Adição das Questões 4 e 5 da Lista Revisão de ATP
    Adicionei as questões 4 e 5 da lista de revisão da matéria de Algoritimos e Técnicas de Programação, completando-a.

---
+++ revisao_atp/q4/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+public class Program{
+    public static int Fatorial(int n){
+        if (n==0){
+            return 1;
+        }
+
+        int fatorial=n;
+        do{
+            fatorial*=n-1;
+            n-=1;
+        } while (n>1);
+        return fatorial;
+    }
+    public static int VerifNum(int n){
+        while (true){
+            if(n<0){
+                Console.WriteLine("Digite um valor maior ou igual a zero: ");
+                n = int.Parse(Console.ReadLine());
+            }
+            else{
+                break;
+            }
+        }
+        return n;
+    }
+    public static void Main(){
+        Console.WriteLine("Digite um valor para calcular o fatorial: ");
+        Console.Write($"O fatorial é: {Fatorial(VerifNum(int.Parse(Console.ReadLine())))}");
+    }
+}
---
+++ revisao_atp/q5/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+public class Program{
+    public static int SomaAcimaDiagPrincip(int[,] matriz, int ordem){
+        int soma=0;
+        Console.Write("Itens acima da diagonal principal: ");
+        for (int i=0; i<matriz.GetLength(0); i++){
+            for (int j=0; j<matriz.GetLength(1); j++){
+                if (i<j){
+                    Console.Write($"{matriz[i, j]}, ");
+                    soma+=matriz[i, j];
+                }
+            }
+        }
+        return soma;
+    }
+    public static void Main(){
+        int ordem;
+        Console.WriteLine("Digite a ordem da matriz quadrada: ");
+        ordem = int.Parse(Console.ReadLine());
+
+        int[,] matriz = new int[ordem, ordem];
+
+        for (int i=0; i<matriz.GetLength(0); i++){
+            for (int j=0; j<matriz.GetLength(1); j++){
+                Console.WriteLine($"Insira um valor na posição [{i},{j}]: ");
+                matriz[i, j] = int.Parse(Console.ReadLine());
+            }
+        }
+
+        Console.WriteLine("A matriz é: ");
+
+        for (int i=0; i<matriz.GetLength(0); i++){
+            for (int j=0; j<matriz.GetLength(1); j++){
+                Console.Write($"{matriz[i, j]} ");
+            }
+            Console.WriteLine();
+        }
+
+        Console.WriteLine($"\nA soma dos itens acima da diagonal principal é: {SomaAcimaDiagPrincip(matriz, ordem)}");
+    }
+}
